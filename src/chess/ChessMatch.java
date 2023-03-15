3127ec9044b9e911584285652d5ba2e2681d35eb
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-03-15 19:40:32 -0300

    Update class

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,11 +24,12 @@ public class ChessMatch {
         return matrice;
     }
 
+    private void placeNewPiece (char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.WHITE), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
-
+        placeNewPiece('b',6, new Rook(board, Color.WHITE));
+        placeNewPiece('d',5, new King(board, Color.WHITE));
     }
 
 }
