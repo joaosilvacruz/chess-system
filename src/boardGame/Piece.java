69b91bd119da89f7e167019ccde6b453c0b2a7e3
João Silva Cruz <100077439+joaosilvacruz@users.noreply.github.com>
2023-03-16 09:51:32 -0300

    Created possible movies for pieces

--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -2,13 +2,30 @@ package boardGame;
 
 import java.util.ArrayList;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
     public Piece(Board board) {
         this.board = board;
         position = null;
+    }
+    public abstract boolean[][] possibleMoves();
+
+    public  boolean possibleMove(Position position){
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove(){
+        boolean[][] matrix = possibleMoves();
+        for (int i=0; i< matrix.length; i++){
+            for (int j=0; j< matrix.length; j++){
+                if (matrix[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     protected Board getBoard(){
